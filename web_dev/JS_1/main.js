102eb75ae3703ac46f80b72cc02ac1cdee066cf5
0anant <bigbluemagic06@gmail.com>
2026-04-11 11:08:13 +0530

    added files

--- web_dev/JS_1/main.js
+++ web_dev/JS_1/main.js
@@ -14,9 +14,15 @@
 
 function sum(n){
     let ans = 0;
-    ans = [n * ( n +1) ] / 2;
+    ans = [n * ( n +1) ];
     return ans;
 }
 
 const ans = sum(10);
 console.log(ans);
+
+//input output heavy operations refers to task in a computer that involves a lot of data transfers between the programs and external systems and devices
+//Examples of input ouptut heavy operations are
+//Readinng a file
+//Starting a clock
+//HTTP Requests
