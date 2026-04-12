8cffd54b2df8c13de43b4dc09e55eb329530ad61
0anant <bigbluemagic06@gmail.com>
2026-04-12 18:02:40 +0530

    added new js file

---
+++ web_dev/JS_2/index.js
@@ -0,0 +1,8 @@
+const map = new Map();
+map.set('name', 'Praveen');
+map.set('age', 26);
+
+const firstName  = map.get('name');
+console.log(firstName);
+const age = map.get('age');
+console.log(age);
