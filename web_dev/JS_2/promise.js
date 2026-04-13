bb94faf9a903b94cfb46cc963a894c38db06480f
0anant <bigbluemagic06@gmail.com>
2026-04-13 22:41:43 +0530

    changes in promise file

--- web_dev/JS_2/promise.js
+++ web_dev/JS_2/promise.js
@@ -1,17 +1,37 @@
-// Promise class gives u a promise, that we will return something in the future;
+// // Promise class gives u a promise, that we will return something in the future;
 // function logName(){
 //     console.log("Praveen");
 // }
 
 // setTimeout(logName, 3000);
-// callback based approach
-//prommise based approach
-//defining a promise is hard
-//using a promise is easy
-function setTimeoutPromisified(ms){
-    return new Promise(resolve => setTimeout(resolve.ms));
+// // callback based approach
+// //prommise based approach
+// //defining a promise is hard
+// //using a promise is easy
+// function setTimeoutPromisified(ms){
+//     return new Promise(resolve => setTimeout(resolve.ms));
+// }
+// function callback(){
+//     console.log("3 seconds have passed");
+// }
+// setTimeoutPromisified(3000).then(callback);
+
+// function waitFor3S(resolve){
+//     setTimeout(resolve, 3000);
+// }
+
+// function main(){
+//     console.log("main is called");
+// }
+
+// waitFor3S(main);
+
+function random(){
+     
 }
+let p = new Promise(random);
+
 function callback(){
-    console.log("3 seconds have passed");
+    console.log("Promise succeeded");
 }
-setTimeoutPromisified(3000).then(callback);
+p.then(callback);
