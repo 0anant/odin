c29ab3306af555024c5472e133a9e30bb0df4530
0anant <bigbluemagic06@gmail.com>
2026-04-14 21:11:34 +0530

    added some js dom files

--- web_dev/dom/index.js
+++ web_dev/dom/index.js
@@ -1,2 +1,22 @@
-const inputVal = document.querySelector('input');
-console.log(inputVal);
+// let ctr = 0;
+// function callback(){
+//     console.log(ctr);
+//     ctr++;
+// }
+
+// setInterval(callback, 1000);
+
+// callback();
+// callback();
+// callback();
+// callback();
+// callback();
+
+
+let ctr = 0;
+function callback(){
+    ctr++;
+    document.querySelector('h5').innerHTML = ctr;
+}
+
+setInterval(callback,1000);
