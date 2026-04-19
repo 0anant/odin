987a2cc7218b77b2312944ab2022316a22fce507
0anant <bigbluemagic06@gmail.com>
2026-04-19 12:22:51 +0530

    new js files

---
+++ pa.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+console.log(__dirname);
+console.log(__dirname + "/index.js");
+console.log(path.join(__dirname, "index.mjs"))
+console.log(__dirname);
--- web_dev/nodeJS/index.mjs
+++ web_dev/nodeJS/index.mjs
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
 
-console.log(chalk.blue('hello Worldd!'));
-console.log(chalk.bold('This is an error message'));
-console.log(chalk.green.underline('This is a success message'));
+console.log(chalk.blue('Hello, World!'));
+console.log(chalk.red.bold('This is an error mmessage.'));
+console.log(chalk.green.underline('This is a success message.'))
